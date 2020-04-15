fb0fcec0ec68bcdb9b43b84ae493388502a28ea8
Ayesh Karunaratne <ayesh@ayesh.me>
2020-04-15 17:50:28 +0700

    Minor code alignment fixes

--- src/StatelessCSRF.php
+++ src/StatelessCSRF.php
@@ -4,89 +4,94 @@ namespace Ayesh\StatelessCSRF;
 
 class StatelessCSRF {
 
-	private const HASH_ALGO = 'sha256';
+    private const HASH_ALGO = 'sha256';
 
-	private $key;
+    private $key;
 
-	private $data = [];
+    private $data = [];
 
-	public function __construct(string $secret_key) {
-		$this->key = $secret_key;
-	}
+    public function __construct(string $secret_key) {
+        $this->key = $secret_key;
+    }
 
-	/**
-	 * Set data that can be used to identify a user. IP address and User-Agent string
-	 * are ideal candiates.
-	 *
-	 * @param string $key
-	 * @param string $value
-	 */
-	public function setGlueData(string $key, string $value): void {
-		$this->data[$key] = $value;
-	}
+    /**
+     * Set data that can be used to identify a user. IP address and User-Agent string
+     * are ideal candidates.
+     *
+     * @param  string  $key
+     * @param  string  $value
+     */
+    public function setGlueData(string $key, string $value): void {
+        $this->data[$key] = $value;
+    }
 
-	public function resetGlue(): void {
-		$this->data = [];
-	}
+    public function resetGlue(): void {
+        $this->data = [];
+    }
 
-	public function getToken(string $identifier, int $expiration = null): string {
-		$seed = $this->getRandomSeed();
-		$hash = $this->generateHash($identifier, $seed, $expiration, $this->data);
-		return $this->urlSafeBase64Encode($seed . '|' . $expiration . '|' . $hash);
-	}
+    public function getToken(string $identifier, int $expiration = null): string {
+        $seed = $this->getRandomSeed();
+        $hash = $this->generateHash($identifier, $seed, $expiration, $this->data);
+        return $this->urlSafeBase64Encode($seed . '|' . $expiration . '|' . $hash);
+    }
 
-	private function getRandomSeed(): string {
-		return $this->urlSafeBase64Encode(random_bytes(8));
-	}
+    private function getRandomSeed(): string {
+        return $this->urlSafeBase64Encode(random_bytes(8));
+    }
 
-	private function urlSafeBase64Encode(string $input): string {
-		$encoded = strtr(base64_encode($input), '+/', '-_');
-		return rtrim($encoded, '=');
-	}
+    private function urlSafeBase64Encode(string $input): string {
+        $encoded = strtr(base64_encode($input), '+/', '-_');
+        return rtrim($encoded, '=');
+    }
 
-	private function generateHash(string $identifier, string $random_seed, int $expiration = null, array $data = []): string {
-		if (null === $expiration) {
-			$expiration = '';
-		}
+    private function generateHash(
+      string $identifier,
+      string $random_seed,
+      int $expiration = null,
+      array $data = []
+    ): string {
+        if (null === $expiration) {
+            /** @noinspection CallableParameterUseCaseInTypeContextInspection */
+            $expiration = '';
+        }
 
-		$identifier = $this->urlSafeBase64Encode($identifier);
-		$props      = [$identifier, $expiration, json_encode($data), $random_seed];
+        $identifier = $this->urlSafeBase64Encode($identifier);
+        $props      = [$identifier, $expiration, json_encode($data, JSON_THROW_ON_ERROR, 512), $random_seed];
 
-		return $this->urlSafeBase64Encode(hash_hmac(static::HASH_ALGO, implode('|', $props), $this->key, true));
-	}
+        return $this->urlSafeBase64Encode(hash_hmac(static::HASH_ALGO, implode('|', $props), $this->key, true));
+    }
 
-	public function validate(string $identifier, string $provided_token, int $current_time = null): bool {
-		$provided_token = $this->urlSafeBase64Decode($provided_token);
-		if (!$provided_token) {
-			return false;
-		}
+    public function validate(string $identifier, string $provided_token, int $current_time = null): bool {
+        $provided_token = $this->urlSafeBase64Decode($provided_token);
+        if (!$provided_token) {
+            return false;
+        }
 
-		$parts = explode('|', $provided_token, 3);
-		if (count($parts) !== 3) {
-			return false;
-		}
+        $parts = explode('|', $provided_token, 3);
+        if (count($parts) !== 3) {
+            return false;
+        }
 
-		if ($parts[1] === '') {
-			$expiration = null;
-		}
-		elseif (!is_numeric($parts[1]) || $current_time > $parts[1]) {
-			return false;
-		}
-		else {
-			$expiration = (int) $parts[1];
-		}
+        if ($parts[1] === '') {
+            $expiration = null;
+        } elseif (!is_numeric($parts[1]) || $current_time > $parts[1]) {
+            return false;
+        } else {
+            $expiration = (int)$parts[1];
+        }
 
-		$hash = $this->generateHash($identifier, $parts[0], $expiration, $this->data);
-		return hash_equals($hash, $parts[2]);
-	}
+        $hash = $this->generateHash($identifier, $parts[0], $expiration, $this->data);
+        return hash_equals($hash, $parts[2]);
+    }
 
-	private function urlSafeBase64Decode(string $input): string {
-		return base64_decode(strtr($input, '-_', '+/'));
-	}
+    private function urlSafeBase64Decode(string $input): string {
+        return base64_decode(strtr($input, '-_', '+/'));
+    }
 
-	public function __debugInfo(): array {
-		return [
-			'data' => $this->data,
-		];
-	}
+    public function __debugInfo(): array {
+        return [
+          'data' => $this->data,
+        ];
+    }
+
 }
--- tests/StatelessCSRFTest.php
+++ tests/StatelessCSRFTest.php
@@ -7,110 +7,111 @@ use PHPUnit\Framework\TestCase;
 
 class StatelessCSRFTest extends TestCase {
 
-	public function testInit(): void {
-		$instance = new StatelessCSRF('test');
-		$this->assertInstanceOf(StatelessCSRF::class, $instance);
-	}
+    public function testInit(): void {
+        $instance = new StatelessCSRF('test');
+        $this->assertInstanceOf(StatelessCSRF::class, $instance);
+    }
 
-	public function testStatelessNoGlue(): void {
-		$key       = bin2hex(random_bytes(8));
-		$generator = new StatelessCSRF($key);
-		$validator = new StatelessCSRF($key);
+    public function testStatelessNoGlue(): void {
+        $key       = bin2hex(random_bytes(8));
+        $generator = new StatelessCSRF($key);
+        $validator = new StatelessCSRF($key);
 
-		$id = bin2hex(random_bytes(8));
-		$this->assertTrue($validator->validate($id, $generator->getToken($id)));
-	}
+        $id = bin2hex(random_bytes(8));
+        $this->assertTrue($validator->validate($id, $generator->getToken($id)));
+    }
 
-	/**
-	 * @dataProvider getValidationDataSet
-	 *
-	 * @param string $key
-	 * @param string $value
-	 * @param string $id
-	 */
-	public function testValidateToken(string $key, string $value, string $id): void {
-		$instance = new StatelessCSRF('test');
-		$instance->setGlueData($key, $value);
-		$result = $instance->getToken($id);
-		$this->assertStringNotContainsString('|', $result);
+    /**
+     * @dataProvider getValidationDataSet
+     *
+     * @param  string  $key
+     * @param  string  $value
+     * @param  string  $id
+     */
+    public function testValidateToken(string $key, string $value, string $id): void {
+        $instance = new StatelessCSRF('test');
+        $instance->setGlueData($key, $value);
+        $result = $instance->getToken($id);
+        $this->assertStringNotContainsString('|', $result);
 
-		$instance->validate($id, $result, time());
-	}
+        $instance->validate($id, $result, time());
+    }
 
-	public function getValidationDataSet(): array {
-		return [
-			['foo', 'bar', 'id'],
-			['foo', 'foo', 'foo'],
-			['foo', 'foo|baz', 'id|baz'],
-			['foo', 'foo||||baz', '||'],
-			['||', '||', '||'],
-		];
-	}
+    public function getValidationDataSet(): array {
+        return [
+          ['foo', 'bar', 'id'],
+          ['foo', 'foo', 'foo'],
+          ['foo', 'foo|baz', 'id|baz'],
+          ['foo', 'foo||||baz', '||'],
+          ['||', '||', '||'],
+        ];
+    }
 
-	/**
-	 * @dataProvider getValidationDataSet
-	 *
-	 * @param string $key
-	 * @param string $value
-	 * @param string $id
-	 *
-	 * @throws \Exception
-	 */
-	public function testSeparateInstanceValidation(string $key, string $value, string $id): void {
-		$secret_key = bin2hex(random_bytes(8));
+    /**
+     * @dataProvider getValidationDataSet
+     *
+     * @param  string  $key
+     * @param  string  $value
+     * @param  string  $id
+     *
+     * @throws \Exception
+     */
+    public function testSeparateInstanceValidation(string $key, string $value, string $id): void {
+        $secret_key = bin2hex(random_bytes(8));
 
-		$generator      = new StatelessCSRF($secret_key);
-		$validator      = new StatelessCSRF($secret_key);
-		$invalid_secret = new StatelessCSRF(str_rot13($secret_key));
+        $generator      = new StatelessCSRF($secret_key);
+        $validator      = new StatelessCSRF($secret_key);
+        $invalid_secret = new StatelessCSRF(str_rot13($secret_key));
 
-		$generator->setGlueData($key, $value);
-		$validator->setGlueData($key, $value);
+        $generator->setGlueData($key, $value);
+        $validator->setGlueData($key, $value);
 
-		$token = $generator->getToken($id);
+        $token = $generator->getToken($id);
 
-		$this->assertTrue($validator->validate($id, $token));
-		$this->assertFalse($invalid_secret->validate($id, $token));
-		$this->assertTrue($validator->validate($id, $token, time() - 1)); // Test tokens without expiration.
-		$this->assertFalse($invalid_secret->validate($id, $token, time() - 1));
+        $this->assertTrue($validator->validate($id, $token));
+        $this->assertFalse($invalid_secret->validate($id, $token));
+        $this->assertTrue($validator->validate($id, $token, time() - 1)); // Test tokens without expiration.
+        $this->assertFalse($invalid_secret->validate($id, $token, time() - 1));
 
-		$validator->resetGlue();
-		$this->assertFalse($validator->validate($id, $token));
-		$this->assertFalse($validator->validate($id, $token, time() - 1));
+        $validator->resetGlue();
+        $this->assertFalse($validator->validate($id, $token));
+        $this->assertFalse($validator->validate($id, $token, time() - 1));
 
-		$new_validator = new StatelessCSRF($secret_key);
-		$new_validator->setGlueData($key, $value);
-		$this->assertTrue($new_validator->validate($id, $token));
-	}
+        $new_validator = new StatelessCSRF($secret_key);
+        $new_validator->setGlueData($key, $value);
+        $this->assertTrue($new_validator->validate($id, $token));
+    }
 
-	/**
-	 * @dataProvider getValidationDataSet
-	 *
-	 * @param string $key
-	 * @param string $value
-	 * @param string $id
-	 *
-	 * @throws \Exception
-	 */
-	public function testTokenExpiration(string $key, string $value, string $id): void {
-		$secret_key = bin2hex(random_bytes(8));
+    /**
+     * @dataProvider getValidationDataSet
+     *
+     * @param  string  $key
+     * @param  string  $value
+     * @param  string  $id
+     *
+     * @throws \Exception
+     */
+    public function testTokenExpiration(string $key, string $value, string $id): void {
+        $secret_key = bin2hex(random_bytes(8));
 
-		$generator = new StatelessCSRF($secret_key);
-		$validator = new StatelessCSRF($secret_key);
+        $generator = new StatelessCSRF($secret_key);
+        $validator = new StatelessCSRF($secret_key);
 
-		$generator->setGlueData($key, $value);
-		$validator->setGlueData($key, $value);
+        $generator->setGlueData($key, $value);
+        $validator->setGlueData($key, $value);
 
-		$time  = time();
-		$token = $generator->getToken($id, $time + 3600);
+        $time  = time();
+        $token = $generator->getToken($id, $time + 3600);
 
-		$this->assertTrue($validator->validate($id, $token, $time));
-		$this->assertTrue($validator->validate($id, $token, $time + 3600));
-	}
+        $this->assertTrue($validator->validate($id, $token, $time));
+        $this->assertTrue($validator->validate($id, $token, $time + 3600));
+    }
 
-	public function testDebugInfoLeakNoSecret(): void {
-		$secret_key = bin2hex(random_bytes(8));
-		$generator  = new StatelessCSRF($secret_key);
-		$val        = print_r($generator, true);
-		$this->assertStringNotContainsString($secret_key, $val);
-	}
+    public function testDebugInfoLeakNoSecret(): void {
+        $secret_key = bin2hex(random_bytes(8));
+        $generator  = new StatelessCSRF($secret_key);
+        $val        = print_r($generator, true);
+        $this->assertStringNotContainsString($secret_key, $val);
+    }
+
 }
